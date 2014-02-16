f9e4414f151ebc4fd9eed0c432b5e54718f64409
sinemusul <sinemusul42@gmail.com>
2014-02-16 22:29:25 +0200

    duzenlendi

--- db/migrate/20140203074453_create_users.rb
+++ db/migrate/20140203074453_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.text :twitter
+      t.string :twitter
 
       t.timestamps
     end
